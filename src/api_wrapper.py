ae3f180f5d5facdbdd2f2d91b01c5b34a1ce80d4
jbmtch <jbmtch31@gmail.com>
2024-11-13 13:17:46 -0800

    Unable to figure out GTFSRT api, going to revisit later

--- src/api_wrapper.py
+++ src/api_wrapper.py
@@ -6,6 +6,39 @@ write basic error handling for API responses (invalid key, bad requests, etc)
 add unit tests for API calls
 
 ensure data is in JSON
+
+{
+  "filter": {
+    "min_delay": 100,
+    "max_delay": 100,
+    "route_id": [
+      "Inland Emp.-Orange Co. Line"
+    ],
+    "stop_id": [
+      "145"
+    ],
+    "stop_name": [
+      "Orange Metrolink Station"
+    ],
+    "trip_id": [
+      "200000030"
+    ],
+    "vehicle_id": [
+      ""
+    ],
+    "vehicle_label": [
+      ""
+    ],
+    "trip_direction_id": [
+      0
+    ],
+    "trip_headsign": [
+      "Laguna Niguel / Mission Viejo"
+    ],
+    "rail_environment": "",
+    "rail_provider": ""
+  }
+}
 '''
 from dotenv import dotenv_values
 
